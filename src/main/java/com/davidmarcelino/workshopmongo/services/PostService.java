a544fedcec1e4b4941bc20910a52eb6224ae3334
David Marcelino <davifernando_10@hotmail.com>
2023-07-17 21:07:43 -0300

    Acrescentando comentários aos posts

---
+++ src/main/java/com/davidmarcelino/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.davidmarcelino.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.davidmarcelino.workshopmongo.domain.Post;
+import com.davidmarcelino.workshopmongo.repository.PostRepository;
+import com.davidmarcelino.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
